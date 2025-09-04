33d75e7fe534ee3fd711b16b95cc168af80ca380
ratulislam46 <arfanratul46@gmail.com>
2025-09-04 23:36:53 +0600

    post new notice data

--- index.js
+++ index.js
@@ -29,40 +29,52 @@ async function run() {
 
 
         const usercollection = client.db('studen-hub').collection('users');
+        const noticeCollection = client.db('studen-hub').collection('notices')
 
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            if (!user) {
-                return res.send({ message: 'users not find' })
-            }
-            const result = await usercollection.insertOne(user);
-            res.send(result)
-        });
 
+        // update user last login time 
         app.patch('/users-last-login/:email', async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
+
             if (!email) {
                 return res.status(400).send({ message: 'user email not found' });
             }
+
+            const filter = { email: email };
             const updateDoc = {
-                $set: {
-                    last_login: new Date().toISOString()
-                }
+                $set: { last_login: new Date().toISOString() }
             }
             const update = await usercollection.updateOne(filter, updateDoc);
+            if (!update.matchedCount === 0) {
+                return res.send({ message: 'No user found with this email' })
+            }
             res.send(update)
         })
-
+        // post new user info 
+        app.post('/users', async (req, res) => {
+            const data = req.body;
+            if (!data) {
+                return res.send({ message: 'users not find' })
+            }
+            const result = await usercollection.insertOne(data);
+            res.send(result)
+        });
+        // get all user 
         app.get('/users', async (req, res) => {
             const userinfo = await usercollection.find().toArray();
             res.send(userinfo)
         })
-
+        // get single user
         app.get('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             res.send(query);
+        })
+        // post new notice data
+        app.post('/add-notice', async (req, res) => {
+            const data = req.body;
+            const addNewNotice = await noticeCollection.insertOne(data);
+            res.send(addNewNotice)
         })
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
